8e5773864fb92151659afd4e38b910f6bd5135b2
mdoyle91 <mike.w.doyle91@gmail.com>
2024-11-06 18:32:17 -0500

    Small Changes, Ready for Submission

--- react-router-dom-lab/src/components/MailboxDetails.jsx
+++ react-router-dom-lab/src/components/MailboxDetails.jsx
@@ -5,18 +5,21 @@ const MailboxDetails = (props) => {
   const singleMailbox = props.mailboxes.find(
     (mailbox) => mailbox._id === Number(mailboxId)
   );
-
-  return (
-    <>
-      <dl className="mail-box">
-        <dt>Mailbox #{singleMailbox._id}</dt>
-        <dt>Box Size:</dt>
-        <dd>{singleMailbox.boxSize}</dd>
-        <dt>Boxholder:</dt>
-        <dd> {singleMailbox.boxholder}</dd>
-      </dl>
-    </>
-  );
+  if (!singleMailbox) {
+    return <h1> Mailbox Not Found... </h1>;
+  } else {
+    return (
+      <>
+        <dl className="mail-box">
+          <dt>Mailbox #{singleMailbox._id}</dt>
+          <dt>Box Size:</dt>
+          <dd>{singleMailbox.boxSize}</dd>
+          <dt>Boxholder:</dt>
+          <dd> {singleMailbox.boxholder}</dd>
+        </dl>
+      </>
+    );
+  }
 };
 
 export default MailboxDetails;
--- react-router-dom-lab/src/components/MailboxForm.jsx
+++ react-router-dom-lab/src/components/MailboxForm.jsx
@@ -2,8 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const initialState = {
-  _id: 0,
-  boxSize: "",
+  boxSize: "Small",
   boxholder: "",
 };
 
@@ -42,8 +41,9 @@ const MailboxForm = (props) => {
           value={formData.boxSize}
           onChange={handleChange}
         >
-          <option>Choose a Box Size</option>
-          <option value="Small">Small</option>
+          <option value="Small" default>
+            Small
+          </option>
           <option value="Medium">Medium</option>
           <option value="Large">Large</option>
         </select>
